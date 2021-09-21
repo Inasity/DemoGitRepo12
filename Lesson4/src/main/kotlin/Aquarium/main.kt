f3af6828438eace9eaa98ffd8243bdfb9cdd68a4
Inasity <lifepunch@hotmail.com>
2021-09-21 12:05:30 -0500

    Finished lesson 4

--- Lesson4/src/main/kotlin/Aquarium/main.kt
+++ Lesson4/src/main/kotlin/Aquarium/main.kt
@@ -6,6 +6,19 @@ fun main (args: Array<String>)
     val spice = SimpleSpice()
     println(spice.name)
     println(spice.heat)
+    val spices = listOf(
+        Spice("curry", "mild"),
+        Spice("pepper", "medium"),
+        Spice("cayenne", "spicy"),
+        Spice("ginger", "mild"),
+        Spice("red curry", "medium"),
+        Spice("green curry", "mild"),
+        Spice("hot pepper", "inferno")
+    )
+    val spice2 = Spice("cayenne", spiciness = "spicy")
+    println("")
+    val spicelist = spices.filter{it.returnHeat() < 5}
+    for (spice in spicelist) spice.showInfo()
 }
 
 fun buildAquarium()
@@ -19,4 +32,14 @@ fun buildAquarium()
     myAquarium.height = 80
     println("Height: ${myAquarium.height} cm")
     println("Volume: ${myAquarium.volume} liters")
+
+    val smallAquarium = Aquarium(length = 20, width = 15, height = 30)
+    println("Small aquarium: ${smallAquarium.volume} liters")
+
+    val myAquarium2 = Aquarium(numberOfFish = 9)
+
+    println("Small Aquarium 2: ${myAquarium2.volume} liters with " +
+    "length ${myAquarium2.length} " +
+    "width ${myAquarium2.width} " +
+    "height ${myAquarium2.height} ")
 }
